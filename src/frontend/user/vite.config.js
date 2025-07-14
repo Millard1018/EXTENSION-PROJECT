5a5ef96b5ddfa7d24b70f9c0b449777a7e2e005b
Millard1018 <155636660+Millard1018@users.noreply.github.com>
2025-07-14 06:18:18 +0000

    commit

--- src/frontend/user/vite.config.js
+++ src/frontend/user/vite.config.js
@@ -3,8 +3,6 @@ import { resolve } from 'path';
 export default {
     root: './src/frontend',
     server: {
-        open: "user/user.html",
-        open: "admin/superadmin.html",
         open: "admin/admin.html",
     },
     build: {
